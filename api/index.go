084286530377b7c68d1eed520cadd6e9839c81ff
Xdahiya <xdahiya@gmail.com>
2024-02-28 21:03:21 +0530

    commit

--- api/index.go
+++ api/index.go
@@ -3,8 +3,29 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-  }
+func Handler() {
+    // New fiber app
+    app := fiber.New()
+
+    // http.Handler -> fiber.Handler
+    app.Get("/", adaptor.HTTPHandler(handler(greet)))
+
+    // http.HandlerFunc -> fiber.Handler
+    app.Get("/func", adaptor.HTTPHandlerFunc(greet))
+
+    // Listen on port 3000
+    app.Listen(":3000")
+}
+
+func handler(f http.HandlerFunc) http.Handler {
+    return http.HandlerFunc(f)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello World!")
+}
